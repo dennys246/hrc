5969a9261700be9b39350fd1c01decd39a48ba23
Denny Schaedig <dennys@wustl.edu>
2025-04-24 10:16:18 -0600

    Adding skeleton analysis scripts for assessing the impact of HRF deconvolution

---
+++ tests/P-CAT_analysis.py
@@ -0,0 +1,21 @@
+import mne, sys, os
+from glob import glob
+from mne_nirs.statistics import run_glm, compute_contrast
+
+# Define runtime variables
+
+# Load in our data
+
+# Clean data if raw
+
+# Extract pertinent labels
+
+# Calculate accuracy?
+
+# Fit GLM
+
+# Contrast congruent vs incongruent trials
+
+# Visualize and save
+
+# Correlate activity to accuracy/response time?

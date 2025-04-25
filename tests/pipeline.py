62c27483668bedc58a9ecfc54a3ecf6194d62008
Denny Schaedig <dennys@wustl.edu>
2025-04-24 20:30:22 -0600

    Compartmentalized all preprocessing functions into a single pipeline that can be called from multiple analysis scripts

---
+++ tests/pipeline.py
@@ -0,0 +1,129 @@
+import mne, scipy
+from glob import glob
+from itertools import compress
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+def load(bids_dir, ex_subs = []):
+    # make a list where all of the scans will get loaded into
+    subject_ids = []
+    raw_scans = []
+    preproc_scans = []
+
+    # Load in master file with scan order info
+    subject_dirs = glob(f'{bids_dir}*/')
+    print(subject_dirs)
+
+    for dir_ind, directory in enumerate(subject_dirs):
+        for excluded in ex_subs:
+            if excluded in directory:
+                print(f"Deleting {directory}")
+                del subject_dirs[dir_ind]
+
+    scan_events = []
+    for subject_dir in subject_dirs:
+
+        snirf_files = glob(f"{subject_dir}/*_Flanker/*.snirf")
+        if len(snirf_files) == 0:
+            continue
+        
+        subject_ids.append(subject_dir.split('/')[-2])
+
+        flanker_dir = '/'.join(snirf_files[0].split('/')[:-1])
+        raw_nirx = mne.io.read_raw_snirf(snirf_files[0])
+        
+        # Grab events
+        events = load_events(flanker_dir)
+        print(f"events: {events}")
+        if len(events) == 0: # If no events
+            print(f"Skipping {subject_dir}, no events found...")
+            continue
+
+        raw_scans.append(raw_nirx)
+        preproc_scans.append(preprocess(raw_nirx))
+        scan_events.append(events)
+        
+    return subject_ids, raw_scans, preproc_scans, scan_events
+
+def load_events(dir):
+    # header =  ['sample', '1', 'block', 'directionality', 'congruency', 'direction', 'response', 'accuracy']
+    # Grab events
+    event_files = glob(f"{dir}/*.evt")
+    print(event_files)
+    for event_file in event_files:
+        if event_file[-8:] == '_old.evt':
+            continue
+        return pd.read_csv(event_file, sep = '\t', header = None, names = ['sample', '0', '1', '2', '3', '4', '5', '6', '7', '8'])
+
+
+def process_congruency(events):
+    """ Process a series of events for P-CAT Flanker and extract congruency """
+    event_id = {
+        'incongruent' : 1,
+        'congruent' : 2
+    }
+    new_events = []
+    for ind, trial in events.iterrows():
+        # Handle no direction case
+        if int(trial['2']) == 0:
+            #new_events.append([sample, 0, 3]) # Append on a event for non-directional
+            continue # exclude non-directional trials
+        else:
+            sample, event = int(trial['sample']), int(trial['3'])
+            event += 1 # Adjust id to match event ID
+            new_events.append([sample, 0 , event])
+    return pd.array(new_events), event_id
+
+def process_accuracy(events):
+    """ Process a series of events for P-CAT Flanker to extract whether an answer was correct or incorrect """
+    event_id = {
+        'incorrect' : 1,
+        'correct' : 2
+    }
+    new_events = []
+    for ind, trial in events.iterrows():
+        # Handle no direction case
+        if int(trial['5']): # If response given (i.e. 1)
+            sample, event = int(trial['sample']), int(trial['6'])
+            event += 1 # Adjust id to match event ID
+            new_events.append([sample, 0 , event])
+    return pd.array(new_events), event_id
+
+
+def preprocess(scan):
+
+    #try:
+    # convert to optical density
+    scan.load_data() 
+    data, times = scan.get_data(return_times=True)
+    plt.plot(times, data[0])
+    plt.savefig('/storage1/fs1/perlmansusan/Active/moochie/github/hrc/tests/plots/raw_data.jpeg')
+    plt.close()
+
+    raw_od = mne.preprocessing.nirs.optical_density(scan)
+
+    # scalp coupling index
+    sci = mne.preprocessing.nirs.scalp_coupling_index(raw_od)
+    raw_od.info['bads'] = list(compress(raw_od.ch_names, sci < 0.5))
+
+    if len(raw_od.info['bads']) > 0:
+        print("Bad channels in subject", raw_od.info['subject_info']['his_id'], ":", raw_od.info['bads'])
+
+    # temporal derivative distribution repair (motion attempt)
+    tddr_od = mne.preprocessing.nirs.tddr(raw_od)
+
+    bp_od = tddr_od.filter(0.01, 0.5)
+
+    # haemoglobin conversion using Beer Lambert Law (this will change channel names from frequency to hemo or deoxy hemo labelling)
+    haemo = mne.preprocessing.nirs.beer_lambert_law(bp_od, ppf=0.1)
+
+    # bandpass filter <-- seems to maj
+    haemo_bp = haemo.copy().filter(
+        0.05, 0.2, h_trans_bandwidth=0.1, l_trans_bandwidth=0.02)
+
+    plt.plot(times, scipy.ndimage.gaussian_filter1d(haemo.get_data()[0], sigma=10))
+    plt.savefig('/storage1/fs1/perlmansusan/Active/moochie/github/hrc/tests/plots/preproc_data.jpeg')
+    plt.close()
+
+    return haemo_bp
